4ee32ae96005710ef09548bbb55865c9fb599dac
TSgt Jason Leaver <jason.leaver@us.af.mil>
2022-10-18 16:33:56 +0000

    updated etl import

--- gswp/etl/__init__.py
+++ gswp/etl/__init__.py
@@ -1,3 +1,15 @@
 __all__ = ["gmgsi", "probsevere"]
+from pathlib import Path
+from datetime import datetime
+from .typing import GMGSIProducts
+from ..etl import _gmgsi, _probsevere
 
-from ..etl import _gmgsi as gmgsi, _probsevere as probsevere
+
+def gmgsi(start: datetime, end: datetime, product: GMGSIProducts, store: Path = None):
+    """download and extract gmgsi data"""
+    _gmgsi.main(start, end, product, store=None)
+
+
+def probsevere(start: datetime, end: datetime, store: Path = None):
+    """download and extract probsevere data"""
+    _probsevere.main(start, end, store)
